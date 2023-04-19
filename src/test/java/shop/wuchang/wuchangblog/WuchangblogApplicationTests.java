139b3d166995a527d723e28b6a84c11847b01673
wuchang <kkang0820qus@gmail.com>
2023-04-19 21:17:23 +0900

    initial commit

---
+++ src/test/java/shop/wuchang/wuchangblog/WuchangblogApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.wuchang.wuchangblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuchangblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

092b8ba92686b30f6f6b6bcf0a826cd70d9171e9
wuchang <kkang0820qus@gmail.com>
2023-04-21 20:08:21 +0900

    1단계 회원가입 뷰 완료

--- src/main/java/shop/wuchang/wuchangblog/WuchangblogApplication.java
+++ src/main/java/shop/wuchang/wuchangblog/WuchangblogApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import shop.wuchang.wuchangblog.model.user.User;
 import shop.wuchang.wuchangblog.model.user.UserRepository;
@@ -11,6 +12,7 @@ import shop.wuchang.wuchangblog.model.user.UserRepository;
 @SpringBootApplication
 public class WuchangblogApplication {
 
+    @Profile("dev")
     @Bean
     CommandLineRunner init(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
         return args -> {

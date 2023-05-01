8a0b19500bea301b085452846221e7159cd32d3b
wuchang <kkang0820qus@gmail.com>
2023-05-01 17:48:35 +0900

    사진 업로드 완료

--- src/main/java/shop/wuchang/wuchangblog/service/UserService.java
+++ src/main/java/shop/wuchang/wuchangblog/service/UserService.java
@@ -1,10 +1,16 @@
 package shop.wuchang.wuchangblog.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import shop.wuchang.wuchangblog.core.exception.ssr.Exception400;
+import shop.wuchang.wuchangblog.core.exception.ssr.Exception500;
+import shop.wuchang.wuchangblog.core.util.MyFileUtil;
 import shop.wuchang.wuchangblog.dto.user.UserRequest;
+import shop.wuchang.wuchangblog.model.user.User;
 import shop.wuchang.wuchangblog.model.user.UserRepository;
 
 @RequiredArgsConstructor
@@ -13,6 +19,9 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;    //securityConfig에등록함
+
+    @Value("${file.path}")
+    private String uploadFolder;
 
     //insert, update, delete -> try catch 처리
     @Transactional
@@ -27,4 +36,24 @@ public class UserService {
         }
 
     }   //더티체킹, DB 세션 종료(OSIV=false)
+
+    public User 회원프로필보기(Long id) {
+        User userPS = userRepository.findById(id)
+                .orElseThrow(()->new Exception400("id", "해당 유저가 존재하지 않습니다"));
+        return userPS;
+    }
+
+    @Transactional
+    public User 프로필사진수정(MultipartFile profile, Long id) {
+        String uuidImageName = MyFileUtil.write(uploadFolder, profile);
+        User userPS = userRepository.findById(id)
+                .orElseThrow(()->new Exception500("로그인 된 유저가 DB에 존재하지 않음"));
+        try {
+            userPS.changeProfile(uuidImageName);
+            return userPS;
+        }catch (Exception e){
+            throw new Exception500("프로필 사진 등록 실패 : "+e.getMessage());
+        }
+    } // 더티체킹 업데이트
+
 }
--- src/main/webapp/WEB-INF/views/layout/header.jsp
+++ src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -54,7 +54,7 @@
             </ul>
             <c:if test="${sessionUser != null}">
                 <div>
-                    <a href="/user/profileUpdate"><img src="/upload/person.png" style="width: 35px;"
+                    <a href="/s/user/${sessionUser.id}/updateProfileForm"><img src="/upload/${sessionUser.profile}" style="width: 35px;"
                                                        class="rounded-circle" alt="Cinque Terre"></a>
                 </div>
             </c:if>
